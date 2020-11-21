ea989ad5a6eaefa4532a00f6316b1ec1577c473d
Orkhan <55583176+OrikMelancholic@users.noreply.github.com>
2020-11-21 12:59:43 +0300

    Commit
    Commit

---
+++ dashboard/dashboard/Config.py
@@ -0,0 +1,20 @@
+# weather
+icon_base_url = 'http://openweathermap.org/img/wn/'
+open_weather_map_api_key = 'd1dca6827af5f4019135638fa04d605a'
+lat = '59.93848'
+lon = '30.312481'
+units = 'metric'
+unit_letter = 'C'
+
+# covid
+country = 'russia'
+covid_url = 'https://corona.lmao.ninja/v2/'
+
+# fonts
+small_font_size = 14
+medium_font_size = 19
+large_font_size = 24
+
+small_font_name = '/home/pi/.fonts/Rubik-Light.ttf'
+medium_font_name = '/home/pi/.fonts/Rubik-Regular.ttf'
+large_font_name = '/home/pi/.fonts/Rubik-Bold.ttf'
